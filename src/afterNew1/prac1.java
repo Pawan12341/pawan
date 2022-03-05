c26f535ddb8727f01d5f39bbcf829c08cbdd1cb4
Lenovo <Lenovo@LAPTOP-F4VJUC3P.mshome.net>
2022-03-05 09:31:14 +0530

    gghh

---
+++ src/afterNew1/prac1.java
@@ -0,0 +1,25 @@
+package afterNew1;
+
+public class prac1 {
+
+	public static void main(String[] args) {
+	   	int a=10;
+        int b=20;
+        int c=a+b;
+	
+        if(c<=10)
+        {
+        	System.out.println("calculation is right");
+        }
+        else
+        {
+        	System.out.println("Calculation is "+c);
+        }
+		
+		
+	
+	
+	
+	}
+
+}
